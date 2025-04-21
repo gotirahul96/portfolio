ec28358af07872eb751f2a950ecc8f1b1a021adf
rahul goti <gotirahul96@gmail.com>
2025-04-20 17:19:47 -0700

    resume-link

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/gotirahul96",
-  resume: "",
+  resume: "https://drive.google.com/file/d/1xewRWI41fmSSpVoG73iCs45ku28pKiRN/view",
   linkedin: "https://linkedin.com/in/rahul-goti",
   twitter: "https://x.com/RGoti74421",
   insta: "https://www.instagram.com/rahulgoti/",
